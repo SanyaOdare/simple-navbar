e61e45a91fc7c6accb87bd05dd4d89a794517a54
Sanya Oluwadare <sanyaodare@gmail.com>
2021-09-13 01:35:54 +0100

    list major steps

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
+// classList - show/gets all classes
+// contains - checks classList for specific class
+// add - add class
+// remove - remove class
+// toggle - toggle class
 
+const navToggle = document.querySelector('.nav-toggle');
+const links = document.querySelector('.links');
+
+navToggle.addEventListener('click', function() {
+  console.log(links.classList);
+})
